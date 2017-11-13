174bcfdecb5bee7364279b6c584b69768406ea6a
root <root@DESKTOP-FK3SQFB.localdomain>
2017-11-12 20:05:34 -0600

    added more code to ExchangeRateReader.java

--- src/xrate/ExchangeRateReader.java
+++ src/xrate/ExchangeRateReader.java
@@ -20,7 +20,8 @@ import org.xml.sax.SAXException;
  * @author carboxylic acid
  */
 public class ExchangeRateReader {
-
+	
+	private String givenURL;
     /**
      * Construct an exchange rate reader using the given base URL. All requests
      * will then be relative to that URL. If, for example, your source is Xavier
@@ -29,12 +30,12 @@ public class ExchangeRateReader {
      * year, month, and day; the URL for 25 June 2010, for example, would be
      * http://api.finance.xaviermedia.com/api/2010/06/25.xml
      *
-     * @param baseURL
+     * @param givenURL
      *            the base URL for requests
      * @throws IOException
      */
-    public ExchangeRateReader(String baseURL) throws IOException {
-      this.baseURL = baseURL;
+    public ExchangeRateReader(String givenURL) throws IOException {
+      this.givenURL = givenURL;
     }
 
     /**
@@ -54,22 +55,21 @@ public class ExchangeRateReader {
      * @throws ParserConfigurationException
      * @throws SAXException
      */
-    public float getExchangeRate(String currencyCode, int year, int month, int day)
+    public float rateofExchange(String currencyCode, int year, int month, int day)
     throws ParserConfigurationException, SAXException, IOException{
 
       //Start an connection
-      String URLaddress = buildURLString(year, month, day);
-      URL url = new URL(urlsting);
+      String URLaddress = createURLString(year, month, day);
+      URL url = new URL(URLaddress);
       InputStream parsStrem = url.openStream();
-      Document parsDoc = createXMLDocument(parsStrem);
-      NodeList exchangeRates = getExchangeRateList(xmlDoc);
-      float rate = getExchangeRate(exchangeRates, currencyCode);
+      Document parsDoc = parseXMLDocument(parsStrem);
+      NodeList exchangeRates = rateofExchangeList(parsDoc);
+      float rate = rateofExchange(exchangeRates, currencyCode);
 
       parsStrem.close();
       return rate;
     }
-
-    /**
+	/**
      * Get the exchange rate of the first specified currency against the second
      * on the specified date.
      *
@@ -86,21 +86,65 @@ public class ExchangeRateReader {
      * @throws ParserConfigurationException
      * @throws SAXException
      */
-    public float getExchangeRate(String fromCurrency, String toCurrency,
+    public float rateofExchange(String donner, String acceptor,
       int year, int month, int day) throws IOException, ParserConfigurationException,
       SAXException {
         //New connection that has the full path name to the xml file that needs to be pars
-        String URLaddress = buildURLString(year, month, day);
-        URL url = new URL(urlsting);
+        String URLaddress = createURLString(year, month, day);
+        URL url = new URL(URLaddress);
         InputStream parsStrem = url.openStream();
         //Generates an xml Document
-        Document parsDoc = createXMLDocument(parsStrem);
+        Document parsDoc = parseXMLDocument(parsStrem);
         // Creates a list of exchange nodes for currency
-        NodeList exchangeRates = getExchangeRateList(parsDoc);
-        float rateCCurrency = getExchangeRate(exchangeRates, rateCCurrency);
-        float pushCurrencyRate = getExchangeRate(exchangeRates, pushCurrencyRate);
+        NodeList exchangeRates = rateofExchangeList(parsDoc);
+        float rateCCurrency = rateofExchange(exchangeRates, donner);
+        float pushCurrencyRate = rateofExchange(exchangeRates, acceptor);
         float rate = rateCCurrency / pushCurrencyRate;
         parsStrem.close();
         return rate;
     }
+
+	private NodeList rateofExchangeList(Document parsDoc) {
+    	return parsDoc.getElementsByTagName("fx");
+	}
+    
+    private float rateofExchange(NodeList exchangeRates, String rateCCurrency) {
+    	float exchangeRate = (float) 1.0;
+    	for (int i =0; i < exchangeRates.getLength(); i++) {
+    		Node mainNode = exchangeRates.item(i);
+    		NodeList childNode = mainNode.getChildNodes();
+    		String cString = childNode.item(1).getTextContent();
+    		String rString = childNode.item(3).getTextContent();
+    		float cRate = Float.parseFloat(rString);
+    		if(cString.equals(rateCCurrency)) {
+    			exchangeRate = cRate;
+    			break;
+    		}
+    		
+    	}
+    	return exchangeRate;
+	}
+
+	private Document parseXMLDocument(InputStream xmlStream) throws ParserConfigurationException, SAXException, IOException{
+		DocumentBuilderFactory buildMain = DocumentBuilderFactory.newInstance();
+    	DocumentBuilder buildDoc = buildMain.newDocumentBuilder();
+    	Document doc = buildDoc.parse(xmlStream);
+    	doc.getDocumentElement().normalize();
+    	
+    	return doc;
+    }
+    private String createURLString(int year, int month, int day) {
+ 		String givenURL = this.givenURL;
+ 		String monthS = String.valueOf(month);
+ 		String dayS = String.valueOf(day);
+ 		if(monthS.length() == 1) {
+ 			monthS = "0" + monthS;
+ 		}
+ 		if(dayS.length() == 1) {
+ 			dayS = "0" + dayS;
+ 		}
+ 		String toReturn = givenURL + year + "/" + monthS + "/" + dayS + ".xml";
+ 		return toReturn;
+ 	}
+
 }
